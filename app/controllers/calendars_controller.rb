bbb42caa08f5d3d1bba92230f05eb16abbb18df2
ko-ta-jpg <fksw122633@gmail.com>
2025-09-24 18:18:55 +0900

    Issue４～６の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -7,29 +7,36 @@ class CalendarsController < ApplicationController
 
   def create
     Plan.create(plan_params)
-
-    redirect_to action: index:
+    redirect_to action: :index
   end
 
   private
 
   def plan_params
-    params.require(calendars:).permit(date:, plan:)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
-    @week_days = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
-    @todays_date = date.today
-   
+    @todays_date = Date.today
+
     @week_days = []
 
-    plans = plan.where(date: @todays_date..@todays_date + 6)
+    plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
+      end 
+      wday_num = @todays_date.wday + x
+      if wday_num > 6
+      wday_num = wday_num -7
       end
+
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wdays: wdays[wday_num]}
       @week_days.push(days)
     end
+  end
+end
